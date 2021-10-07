eafcfc97c888654bf5ee02d9912dfe8f89009262
Maxim <maximka5991@gmail.com>
2021-10-07 13:51:50 +0300

    состояние и жизненный цикл

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -18,7 +18,7 @@ class MarvelService {
         return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacters = async (id) => {
+    getCharacter = async (id) => {
         const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
         return this._transformCharacter(res.data.results[0]);
     }
